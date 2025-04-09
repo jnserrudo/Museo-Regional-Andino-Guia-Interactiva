01b6b07f713701207e3de2e1bdc5c5750c21b195
José Nahuel Serrudo <jnserrudo@ucasal.local>
2025-04-09 15:49:09 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/mra_guia_interactiva/', // 👈 base path del proyecto
+  base: '/mra/guia_interactiva/', // 👈 base path del proyecto
 
 })
