0d48ef7a5d71c4c0f78a7d5bef41acba08ef16f7
José Nahuel Serrudo <jnserrudo@ucasal.local>
2025-07-30 08:16:05 -0300

    fix qr

--- src/Components/SalaMinerologiaMineria.jsx
+++ src/Components/SalaMinerologiaMineria.jsx
@@ -307,7 +307,7 @@ export const SalaMinerologiaMineria = () => {
       ) {
         window.location.href = scannedUrl;
       } else {
-        alert("Este QR no es un enlace válido a un mineral del museo. "+scannedUrl + ' ' + scannedUrl.startsWith("https://qrco.de/bgBght"));
+        alert("El QR no es un enlace válido a un mineral del museo. "+scannedUrl + ' ' + scannedUrl.startsWith("https://qrco.de/bgBght"));
         // Cerramos el escáner si el QR no es válido para que el usuario no se quede atascado.
         setShowQrScanner(false);
       }
