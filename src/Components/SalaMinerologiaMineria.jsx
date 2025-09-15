19fd9fddfcdcdec2c3f6d2dc218165ef7b237b24
José Nahuel Serrudo <jnserrudo@ucasal.local>
2025-09-15 16:00:43 -0300

    fixs txt fluorita

--- src/Components/SalaMinerologiaMineria.jsx
+++ src/Components/SalaMinerologiaMineria.jsx
@@ -139,7 +139,7 @@ const mineralesData = [
     tieneQr: true,
   },
   {
-    id: "fluorita",
+    id: "fluorita_verde",
     img: [
       "/minerales_museo/sin_fondo_blanco/14._Fluorita_verde_1-removebg-preview.png",
       "/minerales_museo/sin_fondo_blanco/14._Fluorita_verde-removebg-preview.png",
